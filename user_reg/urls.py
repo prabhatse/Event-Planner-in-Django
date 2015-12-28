27826e91128eddeaa1f872be9b0ef5ac7df8b387
prabhatse <prabhat.singh88@gmail.com>
2015-12-28 20:45:59 +0300

    Added backend templates and respective urls & views

--- user_reg/urls.py
+++ user_reg/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index_page, name='index_page'),
-    url(r'^dashboard/', views.dashboard, name='dashboard'),
+    url(r'^dashboard/home/', views.dashboard, name='dashboard'),
+    url(r'^dashboard/profile/', views.profile, name='profile'),
+    url(r'^dashboard/events/', views.events, name='events'),
+    url(r'^dashboard/tasks/', views.tasks, name='tasks'),
+    url(r'^dashboard/guests/', views.guests, name='guests'),
+    url(r'^dashboard/budgets/', views.budgets, name='budgets'),
+    url(r'^dashboard/vendors/', views.vendors, name='vendors'),
+    url(r'^dashboard/reviews/', views.reviews, name='reviews'),
+    url(r'^dashboard/inbox/', views.inbox, name='inbox'),
 ]
--- user_reg/views.py
+++ user_reg/views.py
@@ -5,5 +5,29 @@ def index_page(request):
 
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html', {})
+
+def profile(request):
+    return render(request, 'dashboard/profile.html', {})
+
+def events(request):
+    return render(request, 'dashboard/events.html', {})
+
+def tasks(request):
+    return render(request, 'dashboard/tasks.html', {})
+
+def guests(request):
+    return render(request, 'dashboard/guests.html', {})
+
+def budgets(request):
+    return render(request, 'dashboard/budgets.html', {})
+
+def vendors(request):
+    return render(request, 'dashboard/vendors.html', {})
+
+def reviews(request):
+    return render(request, 'dashboard/reviews.html', {})
+
+def inbox(request):
+    return render(request, 'dashboard/inbox.html', {})
    
 
